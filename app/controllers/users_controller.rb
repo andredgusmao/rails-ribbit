620d84912a32c39a09a012d37aef48631312b716
andredgusmao <andredgusmao@gmail.com>
2013-01-19 15:53:02 -0300

    Página de cadastro e dashboard do Usuário

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+	def new 
+		@user = User.new
+	end
+	def create 
+		@user = User.new(params[:user])
+		puts @user
+		if(@user.save)
+			redirect_to @user, notice: "Obrigado por se Cadastrar no Ribbit!"
+		else 
+			render 'new'
+		end
+	end
+	def show
+	  @user = User.find(params[:id])
+	end
 end
